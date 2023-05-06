284905aa2a221d2c27244f46a8c179e2e1fac752
Will <avatec2312@gmail.com>
2023-05-05 23:44:30 -0500

    Now Card uses more data from json

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,17 @@
-export default function Card (props){
+export default function Card ({handleClick,refID,color,altProp,srcUrl}){
 
     const onClick = () =>{
-        props.handleClick(props.refID)
+        handleClick(refID)
     }
 
     return (
         <div>
-            <div 
-            className="card" 
-            style={{width:'100px',height:'100px',backgroundColor:'red',margin:'20px'}}
-            onClick={onClick}
-            >
-                {props.refID}
+            <div className="card">
+                <img alt={altProp}
+                src={srcUrl}
+                onClick={onClick}
+                style={{width:'200px',height:'300px',backgroundColor:color,margin:'30px'}}
+                />
             </div>
         </div>
     )
