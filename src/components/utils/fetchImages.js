db2d3c028ac53874078b2c7b560958827abbd142
Will <avatec2312@gmail.com>
2023-05-06 23:53:58 -0500

    Change algorithms that randomize the selection of imgs

--- src/components/utils/fetchImages.js
+++ src/components/utils/fetchImages.js
@@ -2,11 +2,13 @@ import { createClient } from 'pexels';
 
 const client = createClient('oc6Fo96QjXj8NyhPQsh1zzkp6HT7HfgHL91jjKGM7MVbQp1z0iY2eTJz');
 const query = 'sushi';
-const orientation = 'portrait'
+const orientation = 'portrait';
+const page = Math.floor(Math.random() * 5)
+
 
 export function getImages(){
     // Get sushi images from Api Pexels
-    return client.photos.search({ orientation, query, per_page: 4 }).then(json => {
+    return client.photos.search({ orientation, query, page, per_page: 4 }).then(json => {
         return [...json.photos]
     });
 }
--- src/components/utils/randomize.js
+++ src/components/utils/randomize.js
@@ -1,4 +1,7 @@
-export const randomRef = (array)=>{
+// This shuffles, but the implementation doesnt work as intended, because instead of returning an
+// array it returns an element from that arry
+
+/* export const randomRef = (array)=>{
     let copy = [...array];
     return () => {
         if (copy.length < 1) { copy = [...array]; }
@@ -7,4 +10,18 @@ export const randomRef = (array)=>{
         copy.splice(index, 1);
         return item;
     };
+} */
+
+//It shuffles, but more often than not, get same elements twice in a row, or even fourth in a row
+
+/* export const random = (images)=>{
+    return images.sort(() => Math.random() - 0.5)
+} */
+
+export const random = (array) => {
+        for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
 }
