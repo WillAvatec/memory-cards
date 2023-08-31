d22b83078f2dd0ccdaef93bd7ece10a948e5b412
Will <avatec2312@gmail.com>
2023-08-31 06:13:36 -0500

    Changed file after using linter

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,27 +1,27 @@
-import ScoreDisplay from './Score.js'
-import CardLogic from './CardLogic.js'
-import { useState } from 'react';
+import ScoreDisplay from "./Score.js";
+import CardLogic from "./CardLogic.js";
+import { useState } from "react";
 
-export default function Main (){
-    const [score, setScore] = useState(0);
-    const [highS, setHighS] = useState(0);
-    const scoreLogic = (() => {
-        return {
-            reset:() => setScore(0),
-            up1:() => {
-                setScore(score+1);
-                if(highS <= score) setHighS(highS+1);
-            }
-        }
-    })()
+export default function Main() {
+  const [score, setScore] = useState(0);
+  const [highS, setHighS] = useState(0);
 
+  const scoreLogic = (() => {
+    return {
+      reset: () => setScore(0),
+      up1: () => {
+        setScore(score + 1);
+        if (highS <= score) setHighS(highS + 1);
+      },
+    };
+  })();
 
-    return (
-        <div>
-            <ScoreDisplay score={score} highS={highS}/>
-            <div className="main-wrapper">
-                <CardLogic scoreUpdate={scoreLogic} />
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <ScoreDisplay score={score} highS={highS} />
+      <div className="main-wrapper">
+        <CardLogic scoreUpdate={scoreLogic} />
+      </div>
+    </div>
+  );
 }
