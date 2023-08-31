470310f1a2d1d15fec8b49447b195daaf9558f54
Will <avatec2312@gmail.com>
2023-08-31 06:12:53 -0500

    chore: Fix randomize function.
    It used to return the same positions, because of bad shuffling.

--- src/components/utils/randomize.js
+++ src/components/utils/randomize.js
@@ -1,27 +1,15 @@
-// This shuffles, but the implementation doesnt work as intended, because instead of returning an
-// array it returns an element from that arry
+export const shuffle = (arr) => {
+  // Only grab the last cards
+  const shuffledCards = [...arr.slice(1)];
 
-/* export const randomRef = (array)=>{
-    let copy = [...array];
-    return () => {
-        if (copy.length < 1) { copy = [...array]; }
-        let index = Math.floor(Math.random() * copy.length);
-        let item = copy[index];
-        copy.splice(index, 1);
-        return item;
-    };
-} */
+  // Shuffle them using Fisher-Yates Algorithm
+  for (let i = shuffledCards.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledCards[i], shuffledCards[j]] = [shuffledCards[j], shuffledCards[i]];
+  }
 
-//It shuffles, but more often than not, get same elements twice in a row, or even fourth in a row
+  // Push the first card in the parameters
+  shuffledCards.push(arr[0]);
 
-/* export const random = (images)=>{
-    return images.sort(() => Math.random() - 0.5)
-} */
-
-export const random = (array) => {
-        for (let i = array.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1));
-          [array[i], array[j]] = [array[j], array[i]];
-        }
-        return array;
-}
+  return shuffledCards;
+};
