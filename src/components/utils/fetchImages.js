f4c2800428f9ae40891c5fb2a0978917fd2d7bea
Will <avatec2312@gmail.com>
2023-08-31 06:14:21 -0500

    chore: Add catch to promise chaining

--- src/components/utils/fetchImages.js
+++ src/components/utils/fetchImages.js
@@ -1,14 +1,21 @@
-import { createClient } from 'pexels';
+import { createClient } from "pexels";
 
-const client = createClient('oc6Fo96QjXj8NyhPQsh1zzkp6HT7HfgHL91jjKGM7MVbQp1z0iY2eTJz');
-const query = 'sushi';
-const orientation = 'portrait';
-const page = Math.floor(Math.random() * 5)
+const client = createClient(
+  "oc6Fo96QjXj8NyhPQsh1zzkp6HT7HfgHL91jjKGM7MVbQp1z0iY2eTJz"
+);
+const query = "sushi";
+const orientation = "portrait";
+const page = Math.floor(Math.random() * 5);
 
-
-export function getImages(){
-    // Get sushi images from Api Pexels
-    return client.photos.search({ orientation, query, page, per_page: 4 }).then(json => {
-        return [...json.photos]
+export function getImages(number = 0) {
+  // Get sushi images from Api Pexels
+  return client.photos
+    .search({ orientation, query, page, per_page: 4 + number })
+    .then((json) => {
+      return [...json.photos];
+    })
+    .catch((e) => {
+      console.log("Oops, something went wrong. Check the following error.");
+      console.error(e);
     });
 }
